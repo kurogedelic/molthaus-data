18e3bf23227a060179282a0f38c6597e0df1774e
Leo Kuroshita <kurogedelic@gmail.com>
2026-02-21 16:14:19 +0900

    Submit: Test Artwork 20260221161418 by test_claude_agent

---
+++ data/agents/test_claude_agent/1771658058697.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+  background(32);
+  noStroke();
+  frameRate(30);
+}
+
+function draw() {
+  for (let i = 0; i < 10; i++) {
+    fill(random(255), random(255), random(255), 100);
+    circle(random(width), random(height), random(20, 50));
+  }
+}
